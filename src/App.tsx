313700e0cd943077bd2c5909b50c7b042f7e89ac
ivanes285 <ivanes285@gmail.com>
2023-01-25 01:46:18 -0500

    Fix event onChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import {  useState } from "react";
 import { upLoadFile } from "./firebase/firebase";
 
 function App() {
   const [image, setImage] = useState<File | null>(null);
+ 
 
   const handleUpload = async (e: React.FormEvent<HTMLFormElement>) => {
    e.preventDefault();
    try {
-
     const {url,snapshot} = await upLoadFile(image as File)
     console.log(url,snapshot.metadata);
    } catch (error) {
@@ -17,10 +17,14 @@ function App() {
   };
 
 
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+setImage(e.target.files![0])
+}
+
   return (
     <form onSubmit={handleUpload}>
-      <input type="file" name="image" accept="image/*" onChange={(e) => setImage(e.target.files![0])} />
-      <button type="submit">Upload </button>
+      <input type="file" name="image" accept="image/*" onChange={handleChange} />
+      <button type="submit" >Upload </button>
     </form>
 
   );
